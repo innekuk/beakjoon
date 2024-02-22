ad5229e5ad4336c48c204e1b0d80a707f19624b7
innekuk <monkeygo4628@gamil.com>
2024-02-22 18:40:12 +0900

    9086 answer
    I finished 9086 problem!

---
+++ code/9086.cpp
@@ -0,0 +1,34 @@
+
+
+
+#include<iostream>
+#include<algorithm>
+
+
+using namespace std;
+
+
+int main(){
+	char text[15][105] = {0};
+	int n;
+	scanf("%d" , &n);
+	
+	for (int i = 0; i < n; i++){
+		scanf("%s" , text[n]);
+	}
+
+	for (int i =0 ; i< n ; i++){
+		printf("%c" , text[n][0]);
+		int j = 0; 
+		while(text[n][j] != 0){
+			j++;
+		}
+		printf("%c" , text[n][j-1]);
+	}
+
+	return 0;
+
+}
+
+
+
