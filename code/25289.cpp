12afebab468a75d5f4de8d488ee7ebb0d2097040
innekuk <monkeygo4628@gmail.com>
2024-03-01 18:05:01 +0900

    25289 version 1.0.1
    dd

--- code/25289.cpp
+++ code/25289.cpp
@@ -8,6 +8,8 @@ using namespace std;
 map<int,int> cmap;
 int n;
 int list[100000] = {0};
+int cntlist[100000] = {0};
+
 
 int getlength(int start,int size){
 	int cnt = 1;
@@ -31,18 +33,16 @@ int main(){
 	}
 
 
-	for (int i =0; i<n; i++){
-		for (int j = i +1; j<n; j++){
-			int a = list[j] - list[i];
-			if(cmap[a] == 0 ){
-				cmap[a] = 1;
-				int length = getlength(i,a);
-				if (length > maxlength){
-					maxlength = length;
-				}
+	for (int i = -100; i<=100; i++){
+		for (int j =0 ; j< n; j++){
+			int length = getlength(j , i);
+			if (length > maxlength){
+				maxlength = length;
 			}
 		}
 	}
+
+
 
 	printf("%d" ,maxlength);
 }
