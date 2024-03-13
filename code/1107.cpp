b711dcb8e1c39aff44c7f62e3238e008d51b7006
innekuk <monkeygo4628@gmail.com>
2024-03-14 06:41:08 +0900

    1107 version 1.0.0

---
+++ code/1107.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+
+int check(int val, int * brokenbutton);
+
+int main(){
+	int n;
+	int brokenbutton[10] = {0};
+	int min,max;
+	int cnt;
+	scanf("%d" , &n);
+	int a;
+	scanf("%d" , &a);
+	for (int i =0; i<a; i++){
+		int b;
+		scanf("%d", &b);
+		brokenbutton[b] = 1;
+	}
+	cnt = abs(n-100);
+	if (a==10){
+		printf("%d",cnt);
+		return 0;
+	}
+	int maxcnt;
+	for (max =n; check(max,brokenbutton) ==0 && max <= 500000; max++);
+	if (max <= 500000){
+		maxcnt = check(max,brokenbutton);
+		maxcnt += max-n;
+		if (maxcnt < cnt){
+			cnt = maxcnt;
+		}
+	}
+
+	int mincnt;
+	for (min=n; check(min,brokenbutton) ==0 && min >= 0; min--);
+	if (min >= 0 ){
+		mincnt = check(min,brokenbutton);
+		mincnt += n-min;
+		if (mincnt < cnt){
+			cnt = mincnt;
+		}
+	}
+
+
+	printf("%d",cnt);
+	return 0;
+	
+}
+
+
+
+int check(int val , int * brokenbutton){
+	if (val == 0 && brokenbutton[0] ==0){
+		return 1;
+	}
+	if (val ==0 && brokenbutton[0] == 1){
+		return 0;
+	}
+	int cnt = 0;
+	while(val != 0 ){
+		if (brokenbutton[val%10] == 1){
+			return 0;
+		}
+		val /= 10;
+		cnt++;
+	}
+	return cnt;
+}
+
