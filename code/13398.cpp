1fe14cc8007cfd1d7ae7e63151e7b6511cfb57b4
innekuk <monkeygo4628@gmail.com>
2024-04-06 20:18:43 +0900

    13398 version 4.0.0

--- code/13398.cpp
+++ code/13398.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 #include<algorithm>
-
-
+#include<map>
+#include<vector>
 
 using namespace std;
 
+
+
+map<int,int> dpsubmap;
+map<int,int> dpmaxmap;
 
 int dp[100005] = {0};
 int dpstartpos[100005] = {0};
@@ -12,22 +16,25 @@ int maxdp[100005] = {0};
 int arr[100005] = {0};
 
 
+vector<int> realarr;
+
+
 
 int check(int n){
 	int startpos = 0;
 	int max = arr[0];
 	int submax = arr[0];
-	dp[0] = arr[0];
+	dp[0] = realarr[0];
 	for (int i =1; i< n; i++){
 		if (dp[i-1] > 0){
-			dp[i] = dp[i-1] + arr[i];
+			dp[i] = dp[i-1] + realarr[i];
 		}
 		else{
-			dp[i] = arr[i];
+			dp[i] = realarr[i];
 			for (int j = startpos; j <i-1; j++){
 				maxdp[j] = submax;
 			}
-			maxdp[i-1] = arr[i-1];
+			maxdp[i-1] = realarr[i-1];
 			dpstartpos[i-1] = i-1;
 			startpos = i;
 			submax = arr[i];
@@ -60,23 +67,40 @@ int main(){
 		printf("%d" , arr[0]);
 		return 0;
 	}
-	
-	max = check(n);
+	int sum = 0;
+	for (int i =0; i< n; i++){
+		if (arr[i] < 0 ){
+			if (sum != 0){
+				realarr.push_back(sum);
+			}
+			realarr.push_back(arr[i]);
+			sum =0;
+		}
+		else{
+			sum += arr[i];
+		}
+	}
+	if (arr[n-1] >=0){
+		realarr.push_back(sum);
+	}
+
+
+	max = check(realarr.size());
 	//printf("max : %d\n",max);
-	//for (int i =0; i< n; i++){
-	//	printf("%10d -> dp : %10d ,maxdp : %10d , dpstartpos : %10d\n" , arr[i], dp[i] , maxdp[i] , dpstartpos[i]);
+	//for (int i =0; i< realarr.size(); i++){
+	//	printf("%10d -> dp : %10d ,maxdp : %10d , dpstartpos : %10d\n" , realarr[i], dp[i] , maxdp[i] , dpstartpos[i]);
 	//}
 	int realmax = max;
 	
-	for (int i =0; i<n; i++){
+	for (int i =0; i<realarr.size(); i++){
 		int submax;
 		int subnum;
-		if (arr[i] < 0 && i != 0){
+		if (realarr[i] < 0 && i != 0){
 			//printf("-pos : %d\n" , i);
 			submax = dp[i-1];
 			subnum = dp[i-1];
-			for (int j = i+1 ; j<n && subnum >= 0; j++){
-				subnum += arr[j];
+			for (int j = i+1 ; j<realarr.size() && subnum >= 0; j++){
+				subnum += realarr[j];
 				if (submax < subnum){
 					submax = subnum;
 				}
